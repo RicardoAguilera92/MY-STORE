78eac1fb57170c7078ee18707dac03037163258c
RicardoAguilera92 <ricardo.aguilera92@icloud.com>
2023-07-24 16:36:38 -0600

    midddleware HttpErrors

---
+++ middlewares/error.handler.js
@@ -0,0 +1,11 @@
+function logErrors(err, req, res, next) {
+  console.log
+  console.error(err);
+  next(err);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(500).json({ message: err.message, stack: err.stack,  });
+}
+
+module.exports = { logErrors, errorHandler };
